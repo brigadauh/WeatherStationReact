70c2f4f4331b022a31017f77d784f51c696d3e49
brigadauh <sergey_afanasyev@yahoo.com>
2018-01-07 20:43:49 -0500

    added forecast temp in F

--- src/js/WeatherNow.js
+++ src/js/WeatherNow.js
@@ -31,26 +31,30 @@ var WeatherNow = React.createClass({
           let forecastDateTime=forecasts[i]["forecast_date"];
           let forecastDate=forecastDateTime.substring(0,10);
           let forecast=forecasts[i]["forecasts"];
-          //console.log('forecast',forecasts[i]);
-          if (new Date(forecastDate)>minMaxPeriod) {
-              //console.log('forecastDate',forecastDate);
-              break;
-          }
-          else {
+          //if (new Date(forecastDate)>minMaxPeriod) {
+        //      //console.log('forecastDate',forecastDate);
+        //      break;
+          //}
+          //else {
               let forecastMaxTemp=(Number(forecast.main.temp_max)-273) || -273;
               let forecastMinTemp=(Number(forecast.main.temp_min)-273) || +100;
               if (forecastMaxTemp < forecastMaxTempPrev) {maxStop=true;} // stop when max temperature started to decline
               if (forecastMinTemp > forecastMinTempPrev) {minStop=true;} // stop when min temperature started to rise
               if (forecastMaxTemp > maxTempC && !maxStop ) {maxTempC=forecastMaxTemp; maxTempTime=forecastDateTime;}
               if (forecastMinTemp < minTempC && !minStop) {minTempC=forecastMinTemp; minTempTime=forecastDateTime;}
-          }
+              forecastMaxTempPrev = forecastMaxTemp;
+              forecastMinTempPrev = forecastMinTemp;
+              console.log('forecast',forecastDateTime,forecastMaxTemp,forecastMinTemp );
+          //}
       }
       if (minTempC==100){minTempC="n/a"} else {
-          if (minTempC>0) {minTempC="+"+minTempC.toFixed(0);}
+          minTempC=minTempC.toFixed(0);
+          if (minTempC>0) {minTempC="+"+minTempC;}
           if (minTempC=="-0") {minTempC="0";}
       }
       if (maxTempC==-273){maxTempC="n/a"} else {
-          if (maxTempC>0) {maxTempC="+"+maxTempC.toFixed(0);}
+          maxTempC=maxTempC.toFixed(0);
+          if (maxTempC>0) {maxTempC="+"+maxTempC;}
           if (maxTempC=="-0") {maxTempC="0";}
       }
       let tempC_forecast=(tempTrend==downArrow) ? minTempC : maxTempC;
@@ -71,6 +75,9 @@ var WeatherNow = React.createClass({
                 </div>
                 <div>
                     <span id="min_temp" className="temp">{tempC_forecast}&deg;</span>
+                </div>
+                <div>
+                    <span id="current_temp_2" className="temp-2">&nbsp;{(tempC_forecast*1.8+32).toFixed(0)}<span className="temp-degrees-2">&deg;</span><span id="current_temp_unit_2" className="temp-unit-2">F</span></span>
                 </div>
                 <div className="misc-data datetime">
                     <span id="temp_forecast_time">{tempC_forecast_Time}</span>
